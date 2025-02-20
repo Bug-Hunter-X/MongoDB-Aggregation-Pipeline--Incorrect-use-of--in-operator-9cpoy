afa72e6193c3a8b3672284a77b9e46f1e854b30b
BugHunter-X <askzenai@gmail.com>
2025-02-20 13:00:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+```javascript
+//Correct usage of $in operator in MongoDB aggregation pipeline
+db.collection.aggregate([
+  {
+    $match: {
+      field: { $in: [1, 2, 3] }
+    }
+  },
+  {
+    $group: {
+      _id: "$field",
+      count: { $sum: 1 }
+    }
+  }
+]);
+```
